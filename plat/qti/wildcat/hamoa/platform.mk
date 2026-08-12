bfc057ec1cf588678e6104125ccaf9eb8d9ed072
Aniket Jesu <ajesu@qti.qualcomm.com>
2026-08-12 15:19:40 +0530

    feat(drivers/qti): add family-neutral GIC interrupt shim
    Add a common QTI GIC interface for interrupt registration,
    unregistration, enablement, disablement, and SPI routing.
    
    Implement the Wildcat adapter using the shared EL3 interrupt service.
    Implement the Hoya adapter using callback slots and a trampoline to
    translate between the family-specific ISR signatures.
    
    Integrate the Wildcat adapter into the Hamoa and Nord BL31 builds.
    
    Change-Id: Ia59b40e3f3958ef87b1d46b4bbff6d041e14bdf2
    Signed-off-by: Aniket Jesu <ajesu@qti.qualcomm.com>

--- plat/qti/wildcat/hamoa/platform.mk
+++ plat/qti/wildcat/hamoa/platform.mk
@@ -146,6 +146,7 @@ PLAT_INCLUDES += ${QTI_EXTERNAL_INCLUDES}
 include lib/xlat_tables_v2/xlat_tables.mk
 include drivers/qti/smem/smem.mk
 include drivers/qti/chipinfo/chipinfo.mk
+include drivers/qti/qgic/qgic.mk
 PLAT_BL_COMMON_SOURCES +=					\
 	${XLAT_TABLES_LIB_SRCS}					\
 	plat/common/aarch64/crash_console_helpers.S		\
@@ -188,6 +189,8 @@ BL31_SOURCES +=			\
 	${GIC_SOURCES}		\
 	${TIMER_SOURCES}	\
 	${CPU_SOURCES}
+
+BL31_SOURCES += ${QGIC_DRV_PATH}/qgic_intr_el3.c
 
 LIB_QTI_PATH := ${QTI_PLAT_PATH}/bl31qtilib/lib/${CHIPSET}
 
