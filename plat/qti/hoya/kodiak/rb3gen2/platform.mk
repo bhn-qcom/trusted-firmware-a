523170af04bdd15927352778cc57be83083c5eea
Aniket Jesu <ajesu@qti.qualcomm.com>
2026-08-12 16:49:45 +0530

    feat(drivers/qti): add platform QTimer interrupt support
    Add Hoya and Wildcat QTimer platform services to configure secure
    frame access and register CP15 and QTimer interrupts through QGIC.
    
    Add generic one-shot timer control and interrupt management for CP15
    and memory-mapped timer frames.
    
    Add NCC per-core and cluster sleep timers with interrupt routing,
    state tracking, and synchronized start and cancel operations.
    
    Integrate QTimer with Hamoa, Nord, Lemans EVK, and RB3 Gen2 BL31
    builds.
    
    Change-Id: I5f3a67b73c550bf7ab04f568658c5f12ad532dd1
    Signed-off-by: Aniket Jesu <ajesu@qti.qualcomm.com>

--- plat/qti/hoya/kodiak/rb3gen2/platform.mk
+++ plat/qti/hoya/kodiak/rb3gen2/platform.mk
@@ -107,15 +107,22 @@ $(warning QTISECLIB_PATH is not provided while building, using stub implementati
 
 include drivers/qti/accesscontrol/access_control.mk
 include drivers/qti/smmu/smmu.mk
+include drivers/qti/qgic/qgic.mk
 
 PLAT_INCLUDES	+=	-Iinclude/drivers/qti/sec_core/${CHIPSET} \
 			-Iinclude/drivers/qti/qtimer/${CHIPSET} \
 			-Iinclude/drivers/qti/watchdog/${CHIPSET}
 
+QTI_USE_QTIMER		:=	1
+QTI_USE_NCC_QTIMER	:=	0
+$(eval $(call add_define,QTI_USE_NCC_QTIMER))
+
 BL31_SOURCES	+=	plat/qti/hoya/qtiseclib/src/qtiseclib_interface_stub.c	\
 			drivers/qti/sec_core/sec_core.c				\
 			drivers/qti/sec_core/${CHIPSET}/sec_core_cfg.c		\
+			drivers/qti/qgic/qgic_intr_svc.c				\
 			drivers/qti/qtimer/qtimer.c				\
+			plat/qti/hoya/common/src/qti_qtimer_platform.c		\
 			drivers/qti/watchdog/watchdog.c
 else
 $(eval $(call add_define,QTISECLIB_PATH))
